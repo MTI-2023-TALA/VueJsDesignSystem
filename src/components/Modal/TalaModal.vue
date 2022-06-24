f8bc6708d447651e40719f3ffdcb8d8c83c67679
Alois Heloir <alois.heloir@live.fr>
2022-06-24 10:53:54 +0200

    feat(modal)

--- src/components/Modal/TalaModal.vue
+++ src/components/Modal/TalaModal.vue
@@ -4,21 +4,157 @@ export interface TalaModalProps {
   centered: boolean;
   title: string;
   size: string;
-  "hide-header": boolean;
-  "hide-footer": boolean;
+  hideHeader: boolean;
+  hideFooter: boolean;
 }
 
 withDefaults(defineProps<TalaModalProps>(), {
   showModal: false,
   centered: false,
   size: "md",
-  "hide-header": false,
-  "hide-footer": false,
+  hideHeader: false,
+  hideFooter: false,
 });
 </script>
 
 <template>
-  <div></div>
+  <div class="modal" :class="{ 'is-active': showModal }">
+    <div
+      class="modal-card"
+      :class="{
+        centered: centered,
+        'modal-sm': size === 'sm',
+        'modal-md': size === 'md',
+        'modal-lg': size === 'lg',
+        'modal-xl': size === 'xl',
+      }"
+    >
+      <div class="modal-top" v-if="!hideHeader">
+        <div class="modal-title">
+          {{ title }}
+        </div>
+        <button class="modal-close" v-on:click="$emit('close')">x</button>
+      </div>
+      <div class="divider" v-if="!hideHeader"></div>
+      <div class="modal-body">
+        <slot></slot>
+      </div>
+      <div class="divider" v-if="!hideFooter"></div>
+      <div class="modal-footer" v-if="!hideFooter">
+        <button class="button" v-on:click="$emit('close')">Close</button>
+        <button class="button is-primary" v-on:click="$emit('close')">
+          OK
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import "../../assets/variables";
+.modal {
+  display: none;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.is-active {
+  display: block;
+}
+
+.modal-card {
+  position: absolute;
+  width: 100%;
+  max-width: 500px;
+  background-color: $light-grey-300;
+  border-radius: 0.5rem;
+
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.modal-sm {
+  max-width: 300px;
+}
+
+.modal-md {
+  max-width: 400px;
+}
+
+.modal-lg {
+  max-width: 500px;
+}
+
+.modal-xl {
+  max-width: 800px;
+}
+
+.centered {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.modal-top {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+}
+
+.modal-title {
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: $dark-grey-900;
+}
+
+.modal-close {
+  background-color: transparent;
+  border: none;
+  color: $dark-grey-900;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+
+.modal-body {
+  padding: 0.5rem;
+}
+
+.modal-footer {
+  display: flex;
+  justify-content: flex-end;
+  padding: 0.5rem;
+}
+
+.divider {
+  height: 1px;
+  background-color: $light-grey-500;
+  margin: 0.5rem 0;
+}
+
+.modal-footer button {
+  background-color: $dark-grey-200;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-size: 1rem;
+  padding: 0.25rem 0.5rem;
+  cursor: pointer;
+
+  &.is-primary {
+    background-color: $primary-500;
+    color: white;
+  }
+}
+
+.modal-footer button:first-child {
+  margin-right: 0.5rem;
+}
+</style>
--- src/components/Pages/ModalPage.vue
+++ src/components/Pages/ModalPage.vue
@@ -1,5 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TalaModal from "../Modal/TalaModal.vue";
 
-<template>Work in progress</template>
+let showModal = false;
+function toggleModal() {
+  showModal = !showModal;
+}
+</script>
 
-<style scoped></style>
+<template>
+  <div>
+    <button class="button is-primary" v-on:click="toggleModal">
+      Launch centered modal
+    </button>
+    <TalaModal
+      @close="toggleModal"
+      title="Bonsoir"
+      v-bind:show-modal="showModal"
+      :hide-footer="false"
+      :hide-header="false"
+      :centered="false"
+      size="sm"
+    >
+      <p>Vertically centered modal !</p>
+    </TalaModal>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../assets/variables";
+
+.button {
+  background-color: $dark-grey-200;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-size: 1rem;
+  padding: 0.25rem 0.5rem;
+  cursor: pointer;
+
+  &.is-primary {
+    background-color: $primary-500;
+    color: white;
+
+    &:hover {
+      background-color: $primary-600;
+    }
+  }
+}
+</style>
